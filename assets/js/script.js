41b624c3b85fcbeb75ec68b45a8dece6f347cf4e
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2022-11-21 13:25:45 +0000

    Add code to increment score  and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
 // With the below in place it shows our script file is loading and event handlers are firing
@@ -15,16 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
                 // add an 'if statement' check attribute of data type to see its value
                 // "this" referes to the addition button and what happens when it's clicked
                 if (this.getAttribute("data-type") === "submit") {
-                    alert("You clicked Submit");
+                    checkAnswer();
                 } else {
                     let gameType = this.getAttribute("data-type");
                     runGame(gameType);
                 }
-            })
+            });
         }
 
         runGame("addition");
-})
+});
+
 
 /** 
  * THIS IS A DOCSTRING AND A POP US OF THE DESCRIPTION YOU WRITE IS VISABLE WHEN THE FUNCTION IS CALLED ELSWHERE
@@ -35,8 +36,8 @@ document.addEventListener("DOMContentLoaded", function () {
 function runGame(gameType) {
 
     // Creates two random numbers between 1 and 25
-    let num1 = Math.floor(Math.random() *25 ) + 1;
-    let num2 = Math.floor(Math.random() *25 ) + 1;
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
@@ -48,11 +49,27 @@ function runGame(gameType) {
 }
 
 
-
+/** 
+ * Checks the answer against the first element in 
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
 
-}
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+        incrementScore();
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
+    }
+
+    runGame(calculatedAnswer[1]);
+
+}
 
 /**
  * Gets the operand (the numbers) and the operator (plus, minus etc)
@@ -61,23 +78,35 @@ function checkAnswer() {
 
 // JS gets data from DOM and returns it as a string, using "parseInt" makes sure we treat the value as an interger
 function calculateCorrectAnswer() {
+
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
     let operator = document.getElementById("operator").innerText;
 
     if (operator === "+") {
-        retuen [operand1 + operand2, "addition"];
+        return [operand1 + operand2, "addition"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
     }
     }
 
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 function incrementScore() {
-
+    
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 
 }
 
